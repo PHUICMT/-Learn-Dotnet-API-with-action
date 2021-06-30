ffb2eabf9073e97f44ca7699f1080d7084441d61
PHUICMT <icmtchannel@gmail.com>
2021-06-30 16:47:26 +0700

    add Database

--- PizzaDatabase/Startup.cs
+++ PizzaDatabase/Startup.cs
@@ -10,8 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace PizzaCommand
+namespace PizzaDatabase
 {
     public class Startup
     {
@@ -27,6 +28,10 @@ namespace PizzaCommand
         {
 
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaDatabase", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +40,8 @@ namespace PizzaCommand
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaDatabase v1"));
             }
 
             app.UseHttpsRedirection();
